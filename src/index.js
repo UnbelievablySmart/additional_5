80a21444c1f25606b80c66efc95ad8c1f95bc15b
Unbelievable <slavlen1999@gmail.com>
2018-09-27 21:28:20 +0300

    ready strings

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var stack = [], form = {}, type = {};
+
+  for (var i = 0; i < bracketsConfig.length; i++) {
+    var left = bracketsConfig[i][0];
+    var right = bracketsConfig[i][1];
+    form[left] = right;
+    form[right] = left;
+    type[left] = "opened";
+    type[right] = "closed";
+  }
+
+  for (var i = 0; i < str.length; i++) {
+    var top = stack.length <= 0 ? null : stack[stack.length - 1];
+    var current = str[i];
+
+    if (type[current] == "opened") {
+      stack.push(current);
+    }    
+    else if (type[current] == "closed" && current !== form[current] && stack.length == 0) {
+      return false;
+    }
+    else if (type[current] == "closed" && top !== form[current] && current !== form[current]) {
+      return false;
+    }
+    else if (type[current] == "closed" && top == form[current]) {
+      stack.pop();
+    }
+    else if (type[current] == "closed" && stack.length == 0 && current == form[current]) {
+      stack.push(current);
+    }
+    else if (type[current] == "closed" && stack.length > 0 && top == form[current]) {
+      stack.pop();
+    }
+    else if (type[current] == "closed" && stack.length > 0 && current == form[current]) {
+      stack.push(current);
+    }
+  }
+
+  return (stack.length == 0) ? true : false;
+} 
